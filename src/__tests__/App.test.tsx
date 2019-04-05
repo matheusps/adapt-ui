70705ea0c8870d9e3ea2360469adfcedc28e2e97
matheusps <matheus.ps@icloud.com>
2019-04-05 15:48:59 -0300

    Added Jest

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import App from '../App'
+
+describe('App component', () => {
+  it('should render', () => {
+    const component = render(<App />)
+    expect(component).toBeTruthy()
+  })
+
+  it('shoult match snapshot', () => {
+    const component = render(<App />)
+    expect(component.asFragment()).toMatchSnapshot()
+  })
+})
