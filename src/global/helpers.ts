7e1ef35a8e9c468719b8d2354e9bdc5438bd48c4
matheusps <matheus.ps@icloud.com>
2019-04-13 11:43:42 -0300

    Button sizes

--- src/global/helpers.ts
+++ src/global/helpers.ts
@@ -5,3 +5,14 @@ export const selectTheme = (
   choose?: Color,
   shade?: Shade
 ): string => get(props!, `colors.${choose!}.${shade!}`)
+
+export const getMeasure = (measure: Measure, values: Array<any>) =>
+  measure === 'sm'
+    ? values[0]
+    : measure === 'md'
+    ? values[1]
+    : measure === 'lg'
+    ? values[2]
+    : measure === 'xl'
+    ? values[3]
+    : values[0]
