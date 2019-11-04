f69c66351d5246bf69e675845ef65633bdd1c5e4
matheusps <cout.matheusps@gmail.com>
2019-11-03 23:31:59 -0300

    code preview enabled

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,15 +1,22 @@
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('awesome-typescript-loader'),
-      },
-      {
-        loader: require.resolve('react-docgen-typescript-loader'),
-      },
-    ],
-  })
-  config.resolve.extensions.push('.ts', '.tsx')
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('awesome-typescript-loader'),
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    },
+    {
+      test: /\.stories\.tsx?$/,
+      loaders: [require.resolve('@storybook/source-loader')],
+      enforce: 'pre',
+    }
+  ),
+    config.resolve.extensions.push('.ts', '.tsx')
   return config
 }
